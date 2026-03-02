876981f90c41865c4963268943d074d637fe8f53
Abdullah <ab10102003don@icloud.com>
2026-03-02 10:28:18 +0500

    Feature/4 products page pom (#20)
    * docs(repo): add branching strategy (#1) (#12)
    
    Co-authored-by: Gohar Ellahi <gellahi.dev@gmail.com>
    
    * feat(pom): implement products page object
    
    * feat(pom): implement cart page object
    
    * test(cart): implement product and cart flow tests
    
    ---------
    
    Co-authored-by: Gohar Ellahi <gellahi.dev@gmail.com>

--- src/test/java/com/mobile/automation/tests/LoginTest.java
+++ src/test/java/com/mobile/automation/tests/LoginTest.java
@@ -4,23 +4,16 @@ import com.mobile.automation.base.BaseTest;
 import com.mobile.automation.pages.LoginPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
-
-    @BeforeMethod
-    public void resetApp() {
-        driver.terminateApp("com.saucelabs.mydemoapp.rn");
-        driver.activateApp("com.saucelabs.mydemoapp.rn");
-    }
 
     @Test
     public void validLoginTest() {
 
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.login("bob@example.com", "10203040");
+        loginPage.loginAndVerify("bob@example.com", "10203040");
 
         String productTitle = driver.findElement(
                 By.xpath("//android.view.ViewGroup[@content-desc='products screen']")
